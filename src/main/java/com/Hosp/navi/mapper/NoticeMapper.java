98b2d73d8dfddc734ed1ece13ef12b419bd59771
yeheeshin <zia7894@naver.com>
2024-10-07 19:10:48 +0900

    mapper

---
+++ src/main/java/com/Hosp/navi/mapper/NoticeMapper.java
@@ -0,0 +1,7 @@
+package com.Hosp.navi.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class NoticeMapper {
+}
---
+++ src/main/java/com/Hosp/navi/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.Hosp.navi.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public class UserMapper {
+
+}
