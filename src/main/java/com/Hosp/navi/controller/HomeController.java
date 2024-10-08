40c9e9b279e345b0723bbce37e0ecf6bf7d4e002
yeheeshin <zia7894@naver.com>
2024-10-08 20:33:00 +0900

    react & spring 연결 예제

--- src/main/java/com/Hosp/navi/controller/HomeController.java
+++ src/main/java/com/Hosp/navi/controller/HomeController.java
@@ -1,10 +1,17 @@
 package com.Hosp.navi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class HomeController {
-
+    @GetMapping("/demo-web")
+    public List<String> Hello(){
+        return Arrays.asList("리액트 스프링 ", "연결 성공");
+    }
 }
